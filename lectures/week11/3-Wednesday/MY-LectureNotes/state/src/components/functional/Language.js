aa15db45bf1e21e2a923d77239df36cff6e2b0a8
Joy He <joyhe520@gmail.com>
2022-09-15 16:23:40 -0500

    batman hw and classnote

--- lectures/week11/3-Wednesday/MY-LectureNotes/state/src/components/functional/Language.js
+++ lectures/week11/3-Wednesday/MY-LectureNotes/state/src/components/functional/Language.js
@@ -2,13 +2,33 @@ import React, {useState} from 'react'
 
 function Language() {
     const [language, setLanguage] = useState("🇨🇳")
+    const obj = [
+      {
+      Flag: "🇨🇳",
+      Lang: "Chinese"
+    },
+    {
+      Flag: "🇩🇪",
+      Lang: "German"
+    },
+    {
+      Flag: "🇬🇧",
+      Lang: "English"
+    },
+    {
+      Flag: "🇰🇷",
+      Lang: "Korean"
+    }
+  ]
   return (
     <>
         <select>
-            <option value="Chinese" selected>🇨🇳</option>
+
+          {obj.map(choice => <option value={choice.Lang} onClick={()=> setLanguage(choice.Flag)}>{choice.Flag}</option>)}
+            {/* <option value="Chinese" selected>🇨🇳</option>
             <option value="German" onClick={()=> setLanguage('🇩🇪')}>🇩🇪</option>
             <option value="English">🇬🇧</option>
-            <option value="Korean">🇰🇷</option>
+            <option value="Korean">🇰🇷</option> */}
             
         
         </select>
